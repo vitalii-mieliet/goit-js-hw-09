fe2414c3512e71f70727369a5b40454e1ac4c15b
Will IM <vitalii.mieliet@gmail.com>
2025-05-01 16:58:12 +0300

    Import simplelighbox in js file

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,3 +1,5 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Gallery item template
 `<li class="gallery-item">
   <a class="gallery-link" href="large-image.jpg">

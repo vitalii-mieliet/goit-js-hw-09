49cab008fb7c1faae767a04b531aad4e9fb5cad1
Will IM <vitalii.mieliet@gmail.com>
2025-05-01 08:44:56 +0300

    Set file structure

---
+++ src/js/1-gallery.js
@@ -0,0 +1,6 @@
+// Gallery item template
+`<li class="gallery-item">
+  <a class="gallery-link" href="large-image.jpg">
+    <img class="gallery-image" src="small-image.jpg" alt="Image description" />
+  </a>
+</li>`;
